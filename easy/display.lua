e1fe0eff04e9313fd7d26438213199c2564c70f2
Jerry Lee <cosmos53076@163.com>
2014-11-28 14:22:13 +0800

    add touchCallback for display.newModalLayer

--- easy/display.lua
+++ easy/display.lua
@@ -6,11 +6,12 @@
 local display = {}
 
 -- create modal layer
-function display.newModalLayer(color)
+function display.newModalLayer(color, touchCallback)
 	color = color or cc.c4b(0, 0, 0, 128)
 	local layer = cc.LayerColor:create(color)
 	layer:setTouchEnabled(true)
 	layer:addNodeEventListener(cc.NODE_TOUCH_EVENT, function (event)
+		if touchCallback then touchCallback(event) end
 		return true
 	end)
 	return layer
